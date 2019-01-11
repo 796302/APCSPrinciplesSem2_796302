51dd3c4e1bf2ca9df05e17f3bb864e9e1a0ebc4b
796302 <42678131+796302@users.noreply.github.com>
2019-01-11 08:37:41 -0800

    gawgaeg

--- Lab 109/sort.js
+++ Lab 109/sort.js
@@ -2,15 +2,16 @@ var array = [];
 var Smallsub;
 
 function setup(){
-   for(var i = 0; i < 10; i ++){
+   for(var i = 0; i < 100; i++){
      array[i] = random(10);
 
    }
 
-   selectionSort(array);
+   sort(array);
    console.log(array);
 }
-function selectionSort(array){
+----------Selection Sort----------------
+/*function sort(array){
   for (var i = 0; i < array.length-1; i++){
     var Smallsub = i;
     for(var j = i + 1; j < array.length-1; i++){
@@ -24,8 +25,10 @@ function selectionSort(array){
     temp = array[Smallsub]
   }
 }
-
-/*function insertionSort(array){
+*/
+----------Insertion Sort-----------------
+/*
+function sort(array){
   for(var i = 1; i < array.length; i++){
     for(var j = i; j > 0; i--){
       if(array[i] < array[i-1]){
