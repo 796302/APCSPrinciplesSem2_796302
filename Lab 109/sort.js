6ce82ce7ff6f1b3e4d4f7e6bc53d928c0c9a9b97
796302 <42678131+796302@users.noreply.github.com>
2019-01-14 08:45:01 -0800

    ga

--- Lab 109/sort.js
+++ Lab 109/sort.js
@@ -1,8 +1,10 @@
 var array = [];
 var Smallsub;
-
+var comparisons;
+var swaps;
+var time;
 function setup(){
-   for(var i = 0; i < 100; i++){
+   for(var i = 0; i < 20000; i++){
      array[i] = random(10);
 
    }
@@ -10,6 +12,21 @@ function setup(){
    sort(array);
    console.log(array);
 }
+//-------Bubble Sort-----------
+/*
+function sort(array){
+  for (var i = array.length-1; i > 0; i--){
+    for(var j = 1; i <= i; i++){
+    if([j-1] > [j]){
+      var temp = array[j]
+      array[j] = array[j-1]
+      array[j-1] = temp
+
+  }
+  }
+}
+}
+*/
 //-------Selection Sort----------
 /*
 function sort(array){
