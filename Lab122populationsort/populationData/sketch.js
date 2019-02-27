c1faa5e7bc8ddfc4a68f986b55513c0f34418333
796302 <42678131+796302@users.noreply.github.com>
2019-02-27 08:51:02 -0800

    gaewgaegzgggaeg

--- Lab122populationsort/populationData/sketch.js
+++ Lab122populationsort/populationData/sketch.js
@@ -1,78 +1,109 @@
-var data;
-var barHeight;
-
+var data = [];
 function preload(){
-    loadJSON("population.json", gotData);
+  data = loadJSON("population.json");
 }
 
 
-function setup(){
-  var cnv = createCanvas(800,800);
-  cnv.position((windowWidth-width)/2,30);
-  background(20);
-  barHeight=20;
-  textFont();
-  fill(0,255,0);
-  textAlign(CENTER);
-  textSize(15);
-  text("Pink = females(down arrow).  Blue = males(up arrow)",200,400)
-
-}
-function draw(){
-  //useData();
-  if(keyCode===UP_ARROW){
-    BubbleSort();
-    for(i=0;i<data.countrydata.length;i++){
-      fill(255,0,0)
-      stroke(0,0,255)
-      strokeWeight(6);
-      rect(i,0,2,data.countrydata[i].males/80000)
-    }
-}
-if(keyCode===DOWN_ARROW){
-  insertionSort();
-  for(i=0;i<data.countrydata.length;i++){
-    fill(255,0,0)
-    stroke(0,0,255)
-    strokeWeight(0.5);
-    rect(i,0,2,data.countrydata[i].females/80000)
-  }
-}
+function setup() {
+  //canvas
+  var cnv = createCanvas(1000, 2250);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  //text
+  textSize(10);
+  textAlign(CENTER, CENTER);
+  //calls
+  organize();
 }
 
 
-function gotData(dataNew){
-  data = dataNew;
-
+function draw() {
 }
 
-function BubbleSort(){
-  var length = data.countrydata.length;
-  for(var i = length - 1; i >= 0; i-- ){
-    for(var j = 0; j < i; j++ ){
-      if (data.countrydata[i].males < data.countrydata[j].males){
-        var temp = data.countrydata[i];
-        data.countrydata[i] = data.countrydata[j];
-        data.countrydata[j] = temp
-    }
-  }
-  console.log("BubbleSort")
-  console.log(data);
-}
-  }
 
-
-function insertionSort(){
-  for(var i = 1; i<data.countrydata.length;i++){
-    for(var j = i; j>0;j--){
-      if(data.countrydata[j].females<data.countrydata[j-1].females){
-        var temp = data.countrydata[j];
-        data.countrydata[j]=data.countrydata[j-1];
-        data.countrydata[j-1]=temp;
+function organize(){
+  var temp;
+  for (var i = 1; i < data.countrydata.length; i++){
+    for(var j = i; j > 0; j--){
+      //add change sorting by command
+      if(data.countrydata[j].country < data.countrydata[j-1].country){
+        temp = data.countrydata[j];
+        data.countrydata[j] = data.countrydata[j-1];
+        data.countrydata[j-1] = temp;
       }
     }
-
   }
-  console.log("InsertionSort")
-  console.log(data)
+  drawWords();
+  check();
+  bars();
+}
+
+
+function drawWords(){
+  textAlign(LEFT);
+  //titles
+  fill(255, 255, 255);
+  text("Countries", 100, 10);
+  text("Totals", 300, 10);
+  text("Males", 500, 10);
+  text("Females", 700, 10);
+  //names
+  fill(255, 0, 0);
+  for(var i = 1; i < data.countrydata.length; i++){
+    text(data.countrydata[i].country, 100, 10 + (i*10));
+  }
+  //total
+  fill(0, 255, 0);
+  for(var i = 1; i < data.countrydata.length; i++){
+    text(data.countrydata[i].total, 300, 10 + (i*10));
+  }
+  //male
+  fill(0, 0, 255);
+  for(var i = 1; i < data.countrydata.length; i++){
+    text(data.countrydata[i].males, 500, 10 + (i*10));
+  }
+  //female
+  fill(0, 0, 255);
+  for(var i = 1; i < data.countrydata.length; i++){
+    text(data.countrydata[i].females, 700, 10 + (i*10));
+  }
+}
+
+
+function check(){
+  for(var i = 1; i < data.countrydata.length; i++){
+    console.log(data.countrydata[i].total);
+  }
+}
+
+function bars(){
+  var allTotal = 0;
+  var allMale = 0;
+  var allFemale = 0;
+  for(var i = 1; i < data.countrydata.length; i++){
+    allTotal = allTotal + data.countrydata[i].total;
+    allMale = allMale + data.countrydata[i].males;
+    allFemale = allFemale + data.countrydata[i].females;
+  }
+  //total
+  for(var i = 1; i < data.countrydata.length; i++){
+    var size = (data.countrydata[i].total / allTotal) * (100);
+    console.log(size + "size");
+    fill(0, 255, 0);
+    rect(350, 10 + (i*10), size*5, 10);
+  }
+  //male
+  for(var i = 1; i < data.countrydata.length; i++){
+    var size = (data.countrydata[i].males / allMale) * (100);
+    console.log(size + "size");
+    fill(0, 0, 255);
+    rect(550, 10 + (i*10), size*5, 10);
+  }
+  //female
+  for(var i = 1; i < data.countrydata.length; i++){
+    var size = (data.countrydata[i].females / allFemale) * (100);
+    console.log(size + "size");
+    fill(0, 0, 255);
+    rect(750, 10 + (i*10), size*5, 10);
+  }
 }
