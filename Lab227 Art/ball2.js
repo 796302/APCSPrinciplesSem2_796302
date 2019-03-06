99b649b217913fd63ad6a19da102c24f24cb641a
796302 <42678131+796302@users.noreply.github.com>
2019-03-06 08:47:02 -0800

    work on mouse tracker

--- Lab227 Art/ball2.js
+++ Lab227 Art/ball2.js
@@ -13,21 +13,21 @@ function Ball(location, velocity, radius, col){
 
 
   this.update = function(){
-    if(this !== redBall){
-      var d = this.loc.dist(redBall.loc);
-      if(d > 150){
-        var steeringForce = p5.Vector.sub(redBall.loc, this.loc);
+    if(this !== tMouse){
+      var d = this.loc.dist(tMouse.loc);
+      if(d > 1){
+        var steeringForce = p5.Vector.sub(mouseX, mouseY, this.loc);
         steeringForce.normalize();
         steeringForce.mult(.5);
         this.vel.add(steeringForce);
 
       }
-      if(d < 200){
-        var steeringForce = p5.Vector.sub(this.loc, redBall.loc);
-        steeringForce.normalize();
-        steeringForce.mult(.7);
-        this.vel.add(steeringForce);
-      }
+      // if(d < 200){
+      //   var steeringForce = p5.Vector.sub(this.loc, redBall.loc);
+      //   steeringForce.normalize();
+      //   steeringForce.mult(.7);
+      //   this.vel.add(steeringForce);
+      // }
       this.loc.add(this.vel);
 
     }
@@ -45,14 +45,14 @@ function Ball(location, velocity, radius, col){
     // ellipse(this.loc.x, this.loc.y, this.rad, this.rad);
     var centerVec = createVector(width/2, height/2);
     var dist = this.loc.dist(centerVec);
-    var clrR = map(dist, 0, 500, 30, 50)
-    var clrG = map(dist, 0, 400, 0 , 0)
-    var clrB = map(dist, 0, 200, 20 , 120)
+    var clrR = map(dist, 0, 100, 30, 50)
+    var clrG = map(dist, 0, 100, 0 , 188)
+    var clrB = map(dist, 0, 100, 20 , 60)
     stroke(clrR, clrG, clrB, 150);
-    strokeWeight(3);
-    line(this.loc.x, this.loc.y, redBall.loc.x, redBall.loc.y);
+    strokeWeight(13);
+    // line(this.loc.x, this.loc.y, redBall.loc.x, redBall.loc.y);
 
-    // arc(this.loc.x,this.loc.y,2,50,50,2*Math.PI);
+    arc(this.loc.x,this.loc.y,2,50,50,2*Math.PI);
 
   }
 }//  end of constructor
--- Lab227 Art/sketch.js
+++ Lab227 Art/sketch.js
@@ -1,22 +1,27 @@
 //  Global variables
 var balls = [];
-var redBall;
+// var redBall;
 var toggle;
-
+var xval = [];
+var yval = [];
+var mouse;
 // put setup code here
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(255,255,255);
-  loadBalls(200);
+  loadBalls(600);
+
 }
 
 function draw() {
   if(!toggle){
   background(255, 255, 255, 25);
-  redBall.run();
+  // redBall.run();
   for(var i = 0; i < balls.length; i++){
     balls[i].run();
+
+
   }
 }
 }
@@ -29,12 +34,13 @@ function loadBalls(numBalls){
     var col = color(255, 255, 255);
     balls.push(new Ball(location, velocity, r, col));
   }
-
-  var location = createVector(width/2, height/2);
-  var velocity = createVector(0,0);
-  var r = 0;
-  var col = color(255, 0, 0);
-  redBall = new Ball(location, velocity, r, col);
+var location = (mouseX, mouseY);
+tMouse = new mouse(location);
+  // var location = createVector(width/2, height/2);
+  // var velocity = createVector(0,0);
+  // var r = 0;
+  // var col = color(255, 0, 0);
+  // redBall = new Ball(location, velocity, r, col);
 }
 function mousePressed(){
   toggle = !toggle;
